60c9925989c5b8caebea59b482c5916f58ece0b9
Sylvn001 <juniorbaarros@gmail.com>
2022-04-05 01:41:37 -0300

    feat: created new articles model and controllers ✨

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::ArticlesController < ApiController
+  def index
+
+  end
+end
---
+++ db/migrate/20220405042840_create_launches.rb
@@ -0,0 +1,8 @@
+class CreateLaunches < ActiveRecord::Migration[7.0]
+  def change
+    create_table :launches do |t|
+      t.string :provider
+      t.timestamps
+    end
+  end
+end
